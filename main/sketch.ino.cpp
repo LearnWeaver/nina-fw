91563490dde56a148abe5ba14b5ed773699e0193
Daniel Pollard <daniel.pollard@learnweaver.com>
2021-08-13 23:17:12 +1000

    test serial

--- main/sketch.ino.cpp
+++ main/sketch.ino.cpp
@@ -160,13 +160,11 @@ unsigned long getTime() {
   return ret;
 }
 void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)  {
-
+  Serial.println("Test");
 }
 
 void OnDataRecv(const uint8_t *mac, const uint8_t *data, int len) {
-  //memcpy(&incomingReadings, incomingData, sizeof(incomingReadings));
-  //Serial.print("Bytes received: ");
-  //Serial.println(len);
+  
  
 }
 
